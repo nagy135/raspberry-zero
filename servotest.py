7241b4efe0586d68d9813ec70d8ba001cf442c2a
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-06-12 17:51:54 +0200

    feat: 90deg rotations states

--- servotest.py
+++ servotest.py
@@ -9,12 +9,25 @@ p = GPIO.PWM(11, 50)
 
 p.start(7.5)
 
+state = True
+labels = {
+        True: "water",
+        False: "stop",
+        }
+
+val = {
+        True: 7.5,
+        False: 2.5,
+        }
 try:
     while True:
-        p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-        time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-        time.sleep(1) # sleep 1 second
+        print("===============")
+        print(time.ctime())
+        print(labels[state])
+        state = not state
+
+        p.ChangeDutyCycle(val[state])
+        time.sleep(.3)
 except KeyboardInterrupt:
     p.ChangeDutyCycle(2.5)  # turn towards 0 degree
     p.stop()
